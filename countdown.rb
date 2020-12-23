4899602f15629a39382be86e3adc5745e9311188
Taylor Treece <tayloratreece@gmail.com>
2020-12-23 20:16:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(n)
  end
  return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(n)
+  while n > 0 do
+  puts "#{n} SECOND(S)!"
+  sleep 1
+  n = n - 1
+ end
+ return "HAPPY NEW YEAR!"
+end
