f3ceeb258afe86a01a6067107f13fcba86dbae2f
Taylor Treece <tayloratreece@gmail.com>
2020-12-23 20:17:27 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
 
-def countdown
+
+def countdown(n)
+  while n > 0 do
+  puts "#{n} SECOND(S)!"
+  sleep 1
+  n = n - 1
+ end
+ return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+  while n > 0 do
+  puts "#{n} SECOND(S)!"
+  sleep 1
+  n = n - 1
+ end
+ return "HAPPY NEW YEAR!"
 end
